e14046244fa5c740e94b7fb9ceb4d2a37ce638c4
chenyu <chenyuwuai@gmail.com>
2025-02-27 10:43:33 +0800

    添加项目文件。

---
+++ MFCApplication2/Dib.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+#include "Dib.h"
+
+
+CDib::CDib(void):m_pDibBits(NULL),m_pGrayValueCount(NULL)
+{
+	// initialized variables
+	m_nBitCount = 0;
+	m_nWidth = 0;
+	m_nHeight = 0;
+	m_nWidthBytes = 0;
+
+}
+
+
+CDib::CDib( CDib &Dib ):m_pDibBits(NULL),m_pGrayValueCount(NULL)
+{
+	// initialized variables
+	m_nBitCount = 0;
+	m_nWidth = 0;
+	m_nHeight = 0;
+	m_nWidthBytes = 0;
+
+	if(&Dib == NULL)
+	{
+		return;
+	}
+	if(!IsNull())
+	{
+		Destroy();
+	}
+	Create(Dib.GetWidth(),Dib.GetHeight(),Dib.GetBPP(),0);
+	m_nWidth = Dib.m_nWidth;
+	m_nHeight = Dib.m_nHeight;
+	if(IsIndexed())
+	{
+		int nColors=Dib.GetMaxColorTableEntries();
+		if(nColors > 0)
+		{
+			RGBQUAD* pal{};
+			pal = new RGBQUAD[nColors];
+			Dib.GetColorTable(0,nColors,pal);
+			SetColorTable(0,nColors,pal);
+			delete[] pal;
+		} 
+	}
+	m_nWidthBytes =abs(GetPitch()) ;
+	m_nBitCount = GetBPP();
+	m_pDibBits = (unsigned char*)GetBits()+(m_nHeight-1)*GetPitch();
+	memcpy(m_pDibBits,Dib.m_pDibBits,m_nHeight*m_nWidthBytes);
+}
+
+CDib::~CDib(void)
+{
+	m_pDibBits = NULL;
+	if (m_pGrayValueCount != NULL)
+	{
+		delete []m_pGrayValueCount;
+		m_pGrayValueCount = NULL;
+	}
+}
+
+void CDib::LoadFile( LPCTSTR lpszPathName )
+{
+	Load(lpszPathName);
+	m_nWidth = GetWidth();
+	m_nHeight = GetHeight();
+	m_nWidthBytes =abs(GetPitch()) ;
+	m_nBitCount = GetBPP();
+	m_pDibBits = (unsigned char*)GetBits()+(m_nHeight-1)*GetPitch();
+}
+
+void CDib::Invert()
+{
+	for (int i = 0; i < m_nHeight; i++)
+	{
+		for (int j = 0; j < m_nWidthBytes; j++)
+		{
+			*(m_pDibBits + i*m_nWidthBytes + j) = 255 - *(m_pDibBits + i*m_nWidthBytes + j);
+		}
+	}
+}
+
+long* CDib::GrayValueCount()
+{
+	long nColors = GetMaxColorTableEntries();
+	if (nColors == 0)
+	{
+		return NULL;
+	}
+	long *pGrayValueCount = new long[nColors];
+	memset(pGrayValueCount,0,nColors*sizeof(long));
+	for (int i=0;i<m_nHeight;i++)
+	{
+		for (int j=0;j<m_nWidth;j++)
+		{
+			pGrayValueCount[*(m_pDibBits + i*m_nWidthBytes +j)]++;
+		}
+	}
+	return pGrayValueCount;
+}
+
---
+++ MFCApplication2/Dib.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "atlimage.h"
+
+#include <complex>
+using namespace std;
+
+#define  MAX_SIZE 1000
+const int Smoth_Kernel[9] = {1,1,1,1,1,1,1,1,1};
+#define Pi 3.1415926535897932354626
+#define Ei 2.71828
+
+class CDib :
+	public CImage
+{
+public:
+	CDib(void);
+	CDib(CDib &Dib);
+	~CDib(void);
+
+public:
+	enum chan_color {chan_red=1,chan_green,chan_blue,chan_black};
+	enum HSIChannel{HUE=0,SATURATION,INTENSITY};
+
+public:
+	void LoadFile(LPCTSTR lpszPathName);
+
+public:
+	void Invert();
+
+public:
+	long* GrayValueCount();
+
+private:
+	long m_nWidth;
+	long m_nHeight;
+	int m_nWidthBytes;
+	int m_nBitCount;
+	unsigned char *m_pDibBits;
+	
+private:
+	long *m_pGrayValueCount;
+};
+
